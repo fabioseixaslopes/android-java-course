9762bcb48e5f1c0dd502b3edc8a2e2566e2c863b
Fábio Lopes <fl@uninova.pt>
2022-06-05 17:02:12 +0100

    L022-L023.

--- L022-L023/CurrencyConverter/app/src/main/java/com/fabioseixaslopes/currencyconverter/MainActivity.java
+++ L022-L023/CurrencyConverter/app/src/main/java/com/fabioseixaslopes/currencyconverter/MainActivity.java
@@ -3,6 +3,11 @@ package com.fabioseixaslopes.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void convertToEur(View view){
+        EditText editTextUSD = findViewById(R.id.editTextUsd);
+        if (!editTextUSD.getText().toString().equals("")){
+            double usdValue = Double.parseDouble(editTextUSD.getText().toString());
+            EditText editTextExchangeRate = findViewById(R.id.editTextExchangeRate);
+            double exchangeRate = Double.parseDouble(editTextExchangeRate.getText().toString());
+            double eurValue = usdValue * exchangeRate;
+            Toast.makeText(this, String.format(Locale.getDefault(),"%.2f", eurValue) + " €",Toast.LENGTH_LONG).show();
+        }
     }
 }
--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Developed work for an online android java course on Udemy. An opportunity to rev
 ## Content:
 
 L001 - L021 : Introduction + Android Studio Interface\
-L022 - L023 : Currency Converter App + Quiz 1
+L022 - L023 : Currency Converter App + Quiz 1\
+L024 - L032 : Java Basics
