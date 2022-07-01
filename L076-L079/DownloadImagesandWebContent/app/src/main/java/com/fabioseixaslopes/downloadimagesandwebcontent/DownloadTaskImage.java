acf2f0a02a0eacbad2719ab52176ba97c9281bd4
Fábio Lopes <fl@uninova.pt>
2022-07-02 00:03:37 +0100

    Download web content lesson.

---
+++ L076-L079/DownloadImagesandWebContent/app/src/main/java/com/fabioseixaslopes/downloadimagesandwebcontent/DownloadTaskImage.java
@@ -0,0 +1,40 @@
+package com.fabioseixaslopes.downloadimagesandwebcontent;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class DownloadTaskImage extends AsyncTask<String, Void, Bitmap> {
+    //First parameter is the parameter for the task
+    //Second is for a method for progress of the task
+    //Third is the type of variable returned by the task
+
+    @Override
+    protected Bitmap doInBackground(String... urls) {
+        //access to internet is needed for the app
+        //for that, add the permission to the manifest
+        //<uses-permission android:name="android.permission.INTERNET"/>
+        URL url;
+        HttpURLConnection urlConnection;
+        Bitmap bitmap = null;
+
+        try{
+            //gets URL
+            url = new URL(urls[0]);
+            //opens connection
+            urlConnection = (HttpURLConnection) url.openConnection();
+            //connects, gets input stream and converts to bitmap
+            urlConnection.connect();
+            InputStream inputStream = urlConnection.getInputStream();
+            bitmap = BitmapFactory.decodeStream(inputStream);
+        }
+        catch (Exception e){
+            System.out.println("Error Exception: " + e);
+        }
+        return bitmap;
+    }
+}
--- L076-L079/DownloadImagesandWebContent/app/src/main/java/com/fabioseixaslopes/downloadimagesandwebcontent/MainActivity.java
+++ L076-L079/DownloadImagesandWebContent/app/src/main/java/com/fabioseixaslopes/downloadimagesandwebcontent/MainActivity.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.concurrent.ExecutionException;
+
 public class MainActivity extends AppCompatActivity {
+
+    String testLink = "https://www.fabioseixaslopes.com";
+    String testLink2 = "https://en.wikipedia.org/wiki/Association_football";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Download HTML data from website
+        DownloadTaskTextData downloadTaskTextData = new DownloadTaskTextData();
+        String result = null;
+
+        try {
+            result = downloadTaskTextData.execute(testLink2).get();
+            //with http it does
+            //java.io.IOException: Cleartext HTTP traffic to www.fabioseixaslopes.com not permitted
+            //had to use https
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Content of URL: " + result);
     }
 }
