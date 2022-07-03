329ef465b56cf675125af395f69957d069b8b569
Fábio Lopes <fl@uninova.pt>
2022-07-03 15:29:19 +0100

    Download images lesson.

--- L076-L079/DownloadImagesandWebContent/app/src/main/java/com/fabioseixaslopes/downloadimagesandwebcontent/MainActivity.java
+++ L076-L079/DownloadImagesandWebContent/app/src/main/java/com/fabioseixaslopes/downloadimagesandwebcontent/MainActivity.java
@@ -2,7 +2,11 @@ package com.fabioseixaslopes.downloadimagesandwebcontent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.concurrent.ExecutionException;
 
@@ -10,13 +14,28 @@ public class MainActivity extends AppCompatActivity {
 
     String testLink = "https://www.fabioseixaslopes.com";
     String testLink2 = "https://en.wikipedia.org/wiki/Association_football";
+    String testLink3 = "https://static.wikia.nocookie.net/simpsons/images/6/65/Bart_Simpson.png/revision/latest?cb=20190409004756";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Download HTML data from website
+        // Download Image from link
+        Button downloadImageButton = findViewById(R.id.buttonDownloadImage);
+        ImageView imageViewDownloaded = findViewById(R.id.imageViewDownloaded);
+        downloadImageButton.setOnClickListener(view -> {
+            DownloadTaskImage downloadTask = new DownloadTaskImage();
+            Bitmap image;
+            try {
+                image = downloadTask.execute(testLink3).get();
+                imageViewDownloaded.setImageBitmap(image);
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        // Download HTML data from website
         DownloadTaskTextData downloadTaskTextData = new DownloadTaskTextData();
         String result = null;
 
