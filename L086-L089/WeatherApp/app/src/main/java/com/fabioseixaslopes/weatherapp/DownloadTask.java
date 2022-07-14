23add70c8eedfb8e238de6bd4a201b1c9e2b91fb
Fábio Lopes <fl@uninova.pt>
2022-07-14 15:08:19 +0100

    Weather app.

---
+++ L086-L089/WeatherApp/app/src/main/java/com/fabioseixaslopes/weatherapp/DownloadTask.java
@@ -0,0 +1,84 @@
+package com.fabioseixaslopes.weatherapp;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DecimalFormat;
+
+public class DownloadTask extends AsyncTask<String, Void, String> {
+
+    @SuppressLint("StaticFieldLeak")
+    private TextView cityWeather;
+
+    public DownloadTask(TextView cityWeather) {
+        this.cityWeather = cityWeather;
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        URL url;
+        HttpURLConnection urlConnection;
+        StringBuilder result = new StringBuilder();
+
+        try{
+            url = new URL(strings[0]);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = urlConnection.getInputStream();
+            InputStreamReader reader = new InputStreamReader(in);
+            int data = reader.read();
+            while(data != -1)
+            {
+                char current = (char) data;
+                result.append(current);
+                data = reader.read();
+            }
+        }
+        catch (Exception e){
+            return "failed due to " + e;
+        }
+        return result.toString();
+    }
+
+    @Override
+    //the doInBackground does not interact with the UI, for that use onPostExecute
+    protected void onPostExecute(String s){
+        super.onPostExecute(s);
+        DecimalFormat df = new DecimalFormat();
+        df.setMaximumFractionDigits(2);
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            String weatherInfo = jsonObject.get("weather").toString();
+            String tempInfo = jsonObject.get("main").toString();
+            JSONObject jsonArrayTemp = new JSONObject(tempInfo);
+            JSONArray jsonArray = new JSONArray(weatherInfo);
+            for (int i = 0; i < jsonArray.length(); i++){
+                JSONObject jsonPart = jsonArray.getJSONObject(i);
+                if (!jsonPart.getString("main").equals("") &&
+                        !jsonPart.getString("description").equals("")){
+                    cityWeather.setText("Weather: " +
+                            jsonPart.getString("main") + "\n"
+                            + "Description: " +  jsonPart.getString("description") + "\n"
+                    + "Temperature: " + df.format(kelvinToCelsius(Float.parseFloat(jsonArrayTemp.getString("temp")))) + "ºC" + "\n"
+                            + ".");
+                }
+            }
+        } catch (JSONException e) {
+            cityWeather.setText(R.string.empty_result);
+            e.printStackTrace();
+        }
+    }
+
+    private float kelvinToCelsius(float kelvin){
+        return kelvin - 273.15F;
+    }
+}
--- L086-L089/WeatherApp/app/src/main/java/com/fabioseixaslopes/weatherapp/MainActivity.java
+++ L086-L089/WeatherApp/app/src/main/java/com/fabioseixaslopes/weatherapp/MainActivity.java
@@ -2,13 +2,66 @@ package com.fabioseixaslopes.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+
+    String key;
+    public TextView cityWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get key
+        try {
+            Resources res = getResources();
+            InputStream in_s = res.openRawResource(R.raw.weather_key);
+            Properties properties = new Properties();
+            properties.load(in_s);
+            key = properties.getProperty("api");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        EditText cityEditText = findViewById(R.id.editTextTextCity);
+        cityWeather = findViewById(R.id.textViewWeather);
+        Button button = findViewById(R.id.buttonSubmitCity);
+
+        button.setOnClickListener(view -> {
+            String cityText = cityEditText.getText().toString();
+            if (cityText.equals("")) {
+                cityWeather.setText(getString(R.string.empty_input));
+            }
+            else {
+                //Remove Spaces / Encode (in example San Francisco encodes to San%20Francisco)
+                String encodedCityName = "";
+                try {
+                    encodedCityName = URLEncoder.encode(cityText, "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+
+                DownloadTask task = new DownloadTask(cityWeather);
+                String result = null;
+                try {
+                    result = task.execute("https://api.openweathermap.org/data/2.5/weather?q=" + encodedCityName + "&appid=" + key).get();
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(result);
+            }
+        });
     }
 }
